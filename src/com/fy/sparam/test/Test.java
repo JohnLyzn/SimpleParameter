fd65e6e526a84caf21fe40d2ddab332d04b8ada5
linjie <434451202@qq..com>
2017-08-26 04:06:43 +0800

    去除没用的工具类

--- src/com/fy/sparam/test/Test.java
+++ src/com/fy/sparam/test/Test.java
@@ -31,7 +31,7 @@ public class Test {
 		
 		CustomerParameter cParam = SqlParameter.getParameter(CustomerParameter.class);
 //		CustomerParameter cParam1 = (CustomerParameter) cParam.clone();
-		cParam.email.eq("1").and(cParam);
+		cParam.email.eq("1");
 		System.out.println(cParam.build().getSql());
 	}
 }
